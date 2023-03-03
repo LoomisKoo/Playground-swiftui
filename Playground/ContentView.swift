aa3ff11194f1fb32d266fe03cc368d8a2063bb6a
古春雄 <chunxiong-gu@msyc.cc>
2023-03-03 14:28:21 +0800

    Initial Commit

---
+++ Playground/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Playground
+//
+//  Created by Loomis Koo on 2023/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Loomis Koo on 2023/3/3.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
